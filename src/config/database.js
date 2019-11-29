f6faa6fe990357987a689b96401a9646f59b992a
EduardoAraujoB <ea0123450@gmail.com>
2019-11-28 21:18:49 -0300

    configuring sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'qwerty',
+  database: 'moovies-catalogs',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
