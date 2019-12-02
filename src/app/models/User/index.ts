9af88b73093bd955cf2ec6f748420ec3c7d61acf
EduardoAraujoB <ea0123450@gmail.com>
2019-12-02 14:02:15 -0300

    added some configs to the project linters

--- src/app/models/User/index.ts
+++ src/app/models/User/index.ts
@@ -1,26 +1,27 @@
-import {
-  Sequelize, Model, DataTypes,
-} from 'sequelize';
+import { Sequelize, Model, DataTypes } from 'sequelize';
 
 class User extends Model {
-  public static initialize(sequelize: Sequelize) : void{
-    User.init({
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
+  public static initialize(sequelize: Sequelize): void {
+    User.init(
+      {
+        name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        email: {
+          type: DataTypes.STRING,
+          allowNull: true,
+        },
+        password_hash: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
       },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: true,
+      {
+        sequelize,
+        tableName: 'users',
       },
-      password_hash: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-    }, {
-      sequelize,
-      tableName: 'users',
-    });
+    );
   }
 }
 
--- src/database/index.ts
+++ src/database/index.ts
@@ -1,4 +1,4 @@
-import Sequelize from 'sequelize';
+import { Sequelize } from 'sequelize';
 
 import User from '../app/models/User';
 
@@ -7,7 +7,7 @@ import databaseConfig from '../config/database';
 const models = [User];
 
 class Database {
-  connection: Sequelize.Sequelize;
+  connection: Sequelize;
 
   constructor() {
     this.init();
