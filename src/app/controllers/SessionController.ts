fe8f72541724c64a9d258149570fa2f1c1a87669
EduardoAraujoB <ea0123450@gmail.com>
2019-12-11 13:44:48 -0300

    created validation for data in requests

--- src/app/controllers/SessionController.ts
+++ src/app/controllers/SessionController.ts
@@ -1,11 +1,23 @@
 import { Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
 
 import User from '../models/User';
 import authConfig from '../../config/auth';
 
 class SessionController {
   async store(req: Request, res: Response): Promise<Response> {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation Fails' });
+    }
+
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email } });
--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -1,9 +1,24 @@
 import { Request, Response } from 'express';
+import * as Yup from 'yup';
 
 import User from '../models/User';
 
 class UserController {
   public async store(req: Request, res: Response): Promise<Response> {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation Fails' });
+    }
+
     const checkEmail = await User.findOne({ where: { email: req.body.email } });
 
     if (checkEmail) {
